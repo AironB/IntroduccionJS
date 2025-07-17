12c32f20b40f5b90fbe5882ecf47650204e482c7
AironB <d17ex02@outlook.es>
2025-07-16 19:51:20 -0600

    cambios

---
+++ JS/01.js
@@ -0,0 +1,24 @@
+//variables
+
+var producto = 'Audifonos Gamer';//Iniciar variable y asignarle valor
+
+var disponible; //iniciamos la variable pero sin valor
+
+producto = true;
+
+disponible = true;
+
+var product1 = 'Computadora',
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// var 1disponible;
+// var -disponible;
+
+//estilos para las variables
+var nombre_producto = 'Monitor HD'; //underscore
+var nombreProducto = 'Monitor HD'; //Camelcase
+var NombreProducto = 'Monitor HD'; //Pascal Case
+var nombreproducto = 'Monitor HD';
+
+console.log(producto);
